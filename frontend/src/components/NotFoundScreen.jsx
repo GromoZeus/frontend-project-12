d0f5cf32ede79eb6b1df23dc43924403c0650c10
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-08-26 19:36:13 +0700

    deleted files

--- frontend/src/components/NotFoundScreen.jsx
+++
@@ -1,8 +0,0 @@
-const NotFoundScreen = () => (
-  <div id="root">
-    <h2>Unexpected Application Error!</h2>
-    <h3 style={{ fontStyle: 'italic' }}>404 Not Found</h3>
-  </div>
-)
-
-export default NotFoundScreen
