b7c102d7470512a03fa87d8922e7d890d1d10f78
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-08-26 22:28:37 +0700

    modified Routes.jsx

--- frontend/src/components/Routes.jsx
+++ frontend/src/components/Routes.jsx
@@ -1,15 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom'
-import getRoutes from '../routes.js'
+import getPath from '../path.js'
+import LogedIn from './LogedIn.jsx'
 import LoginPage from './LoginPage.jsx'
 import NotFoundPage from './NotFoundPage.jsx'
 
 const router = createBrowserRouter([
   {
-    path: getRoutes.chatPagePath(),
-    element: <span>Chat</span>,
+    path: getPath.chatPagePath(),
+    element: <LogedIn />,
   },
   {
-    path: getRoutes.loginPagePath(),
+    path: getPath.loginPagePath(),
     element: <LoginPage />,
   },
   {
