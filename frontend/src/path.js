7ab8f3f19838d16f8d18c24a98014fe5f0b8de74
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-08-29 17:39:38 +0700

    add in path string channelsPath

--- frontend/src/path.js
+++ frontend/src/path.js
@@ -2,7 +2,7 @@ const apiPath = '/api/v1'
 
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
-  // usersPath: () => [apiPath, 'data'].join('/'),
+  channelsPath: () => [apiPath, 'channels'].join('/'),
   chatPagePath: () => '/',
   loginPagePath: () => '/login',
 }
