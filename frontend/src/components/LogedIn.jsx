24d4a4da141e2ce06b26dff261b951385d166698
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-08-30 20:54:04 +0700

    import LogedIn component ChatPage

--- frontend/src/components/LogedIn.jsx
+++ frontend/src/components/LogedIn.jsx
@@ -1,6 +1,6 @@
 import { Navigate, useLocation } from 'react-router-dom'
 import useAuth from '../hooks/index.jsx'
-// import ChatPage from './componentsChat/ChatPage.jsx'
+import ChatPage from './ChatPage.jsx'
 import getPath from '../path.js'
 
 const LogedIn = () => {
@@ -8,7 +8,7 @@ const LogedIn = () => {
   const location = useLocation()
   return (
     auth.loggedin
-      ? (<span>Chat</span>)
+      ? (<ChatPage />)
       : (
           <Navigate
             to={getPath.loginPagePath()}
