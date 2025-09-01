f72ebe77bc108fff7c2899ab83c6f51beb0b6122
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-09-01 18:05:23 +0700

    add message path for API

--- frontend/src/path.js
+++ frontend/src/path.js
@@ -3,6 +3,7 @@ const apiPath = '/api/v1'
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
   channelsPath: () => [apiPath, 'channels'].join('/'),
+  messagesPath: () => [apiPath, 'messages'].join('/'),
   chatPagePath: () => '/',
   loginPagePath: () => '/login',
 }
