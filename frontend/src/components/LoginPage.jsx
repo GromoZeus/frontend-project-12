94e145afe449a01f022844ef3e17cf0334ab55c9
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-09-01 18:09:19 +0700

    add logic for authorization in LoginPage

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import axios from 'axios'
 import { useEffect, useRef, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { useFormik } from 'formik'
 import { Button, Form, FloatingLabel } from 'react-bootstrap'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { actions } from '../slices/index.js'
 import useAuth from '../hooks/index.jsx'
 import getPath from '../path.js'
 
 const LoginPage = () => {
   const auth = useAuth()
+  const dispatch = useDispatch()
   const [authFailed, setAuthFailed] = useState(false)
   const inputRef = useRef()
   const location = useLocation()
@@ -30,6 +33,7 @@ const LoginPage = () => {
       try {
         const res = await axios.post(getPath.loginPath(), values)
         localStorage.setItem('userId', JSON.stringify(res.data))
+        dispatch(actions.authorization({ name: res.data.username, token: res.data.token }))
         auth.login(res.data)
         const { from } = location.state || { from: { pathname: '/' } }
         navigate(from)
@@ -50,7 +54,7 @@ const LoginPage = () => {
     <div className="container-fluid">
       <div className="row justify-content-center pt-5">
         <div className="col-sm-4">
-          <Form onSubmit={formik.handleSubmit} classNclassName="col-12 col-md-6 mt-3 mt-mb-0">
+          <Form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
             <fieldset>
               <Form.Group className="form-floating mb-3">
                 <FloatingLabel controlId="username" label="Ваш ник">
@@ -62,7 +66,7 @@ const LoginPage = () => {
                     disabled={formik.isSubmitting}
                     placeholder="username"
                     name="username"
-                    id="username"
+                    // id="username"
                     autoComplete="username"
                     isInvalid={authFailed}
                     required
@@ -80,13 +84,13 @@ const LoginPage = () => {
                     disabled={formik.isSubmitting}
                     placeholder="password"
                     name="password"
-                    id="password"
+                    // id="password"
                     autoComplete="current-password"
                     isInvalid={authFailed}
                     required
                   />
                 </FloatingLabel>
-                <Form.Control.Feedback type="invalid" className="invalid-feedback">the username or password is incorrect</Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid" className="invalid-feedback" style={{ display: authFailed ? 'block' : 'none' }}>the username or password is incorrect</Form.Control.Feedback>
               </Form.Group>
               <Button type="submit" disabled={formik.isSubmitting} variant="outline-primary" className="w-100 mb-3">Войти</Button>
             </fieldset>
