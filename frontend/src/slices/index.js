2805b63d1a436169c7fb0a08538eab13c37d2bb3
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-09-01 18:08:24 +0700

    modified index add authorizationState

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,12 +1,15 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import authorizationState, { actions as authorizationStateActions } from './authorizationState.js'
 import channelsState, { actions as channelsStateActions } from './channelsState.js'
 import messagesState, { actions as messagesStateActions } from './messagesState.js'
 
 const reducer = combineReducers({
+  authorizationState,
   channelsState,
   messagesState,
 })
 export const actions = {
+  ...authorizationStateActions,
   ...channelsStateActions,
   ...messagesStateActions,
 }
