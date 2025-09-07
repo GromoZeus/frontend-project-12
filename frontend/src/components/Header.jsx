dc4ed2b592d945b3187bbdbb769bf9fc8fa03ef8
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-09-07 20:04:38 +0700

    add Header commponent, refactor Routes

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import { Button, Navbar, Container } from 'react-bootstrap'
+// import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+
+import { useAuth } from '../hooks/index.js'
+import getPath from '../path.js'
+
+const AuthButton = () => {
+  const auth = useAuth()
+  // const { t } = useTranslation()
+  return (
+    auth.loggedin
+      ? <Button onClick={auth.logout}>exitButton</Button>
+      : null
+  )
+}
+
+const Header = () => {
+  // const { t } = useTranslation()
+  return (
+    <Navbar bg="white" expand="lg" className="shadow-sm">
+      <Container>
+        <Navbar.Brand as={Link} to={getPath.chatPagePath()}>chatLogo</Navbar.Brand>
+        <AuthButton />
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header
--- frontend/src/components/Routes.jsx
+++ frontend/src/components/Routes.jsx
@@ -2,20 +2,32 @@ import { createBrowserRouter } from 'react-router-dom'
 import getPath from '../path.js'
 import LogedIn from './LogedIn.jsx'
 import LoginPage from './LoginPage.jsx'
+import SignupPage from './SignupPage.jsx'
 import NotFoundPage from './NotFoundPage.jsx'
+import Layout from './Layout.jsx'
 
 const router = createBrowserRouter([
   {
-    path: getPath.chatPagePath(),
-    element: <LogedIn />,
-  },
-  {
-    path: getPath.loginPagePath(),
-    element: <LoginPage />,
-  },
-  {
-    path: '*',
-    element: <NotFoundPage />,
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: getPath.chatPagePath(),
+        element: <LogedIn />,
+      },
+      {
+        path: getPath.loginPagePath(),
+        element: <LoginPage />,
+      },
+      {
+        path: getPath.signupPagePath(),
+        element: <SignupPage />,
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />,
+      },
+    ],
   },
 ])
 
