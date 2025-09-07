2c5bb3af43f24a8029af927fdcec7203c177025a
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-09-07 19:52:36 +0700

    modified vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['react-bootstrap', 'bootstrap'],
+          form: ['formik', 'yup'],
+          utils: ['axios'],
+          // utils: ['axios', 'lodash', 'leo-profanity'],
+          // Добавьте другие тяжелые библиотеки
+        },
+      },
+    },
+  },
   plugins: [react()],
   server: {
     port: 5002,
