d98c7e89a8204f52989ba2fd1cd41de751e3dc29
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-09-07 20:05:24 +0700

    add SignupPage component and signupPath function

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -3,10 +3,10 @@ import axios from 'axios'
 import { useEffect, useRef, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useFormik } from 'formik'
-import { Button, Form, FloatingLabel } from 'react-bootstrap'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { Button, Form, FloatingLabel, Container, Row, Col, Card } from 'react-bootstrap'
+import { useLocation, useNavigate, NavLink } from 'react-router-dom'
 import { actions } from '../slices/index.js'
-import { useAuth } from '../hooks/index.jsx'
+import { useAuth } from '../hooks/index.js'
 import getPath from '../path.js'
 
 const LoginPage = () => {
@@ -51,53 +51,71 @@ const LoginPage = () => {
   })
 
   return (
-    <div className="container-fluid">
-      <div className="row justify-content-center pt-5">
-        <div className="col-sm-4">
-          <Form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
-            <fieldset>
-              <Form.Group className="form-floating mb-3">
-                <FloatingLabel controlId="username" label="Ваш ник">
-                  <Form.Control
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.username}
-                    onBlur={formik.handleBlur}
-                    disabled={formik.isSubmitting}
-                    placeholder="username"
-                    name="username"
-                    // id="username"
-                    autoComplete="username"
-                    isInvalid={authFailed}
-                    required
-                    ref={inputRef}
-                  />
-                </FloatingLabel>
-              </Form.Group>
-              <Form.Group className="form-floating mb-3">
-                <FloatingLabel controlId="password" label="Пароль">
-                  <Form.Control
-                    type="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                    onBlur={formik.handleBlur}
-                    disabled={formik.isSubmitting}
-                    placeholder="password"
-                    name="password"
-                    // id="password"
-                    autoComplete="current-password"
-                    isInvalid={authFailed}
-                    required
-                  />
-                </FloatingLabel>
-                <Form.Control.Feedback type="invalid" className="invalid-feedback" style={{ display: authFailed ? 'block' : 'none' }}>the username or password is incorrect</Form.Control.Feedback>
-              </Form.Group>
-              <Button type="submit" disabled={formik.isSubmitting} variant="outline-primary" className="w-100 mb-3">Войти</Button>
-            </fieldset>
-          </Form>
-        </div>
-      </div>
-    </div>
+    <Container className="h-100" fluid>
+      <Row className="justify-content-center align-content-center h-100">
+        <Col className="col-12 col-md-8 col-xxl-6">
+          <Card className="shadow-sm">
+            <Card.Body className="p-5 row">
+              <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                {/* <img
+                  src={imagePath}
+                  className="roundedCircle"
+                  alt="Log in page"
+                /> */}
+              </div>
+              <Form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
+                <fieldset>
+                  <Form.Group className="form-floating mb-3">
+                    <FloatingLabel controlId="username" label="Ваш ник">
+                      <Form.Control
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.username}
+                        onBlur={formik.handleBlur}
+                        disabled={formik.isSubmitting}
+                        placeholder="username"
+                        name="username"
+                        // id="username"
+                        autoComplete="username"
+                        isInvalid={authFailed}
+                        required
+                        ref={inputRef}
+                      />
+                    </FloatingLabel>
+                  </Form.Group>
+                  <Form.Group className="form-floating mb-3">
+                    <FloatingLabel controlId="password" label="Пароль">
+                      <Form.Control
+                        type="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        onBlur={formik.handleBlur}
+                        disabled={formik.isSubmitting}
+                        placeholder="password"
+                        name="password"
+                        // id="password"
+                        autoComplete="current-password"
+                        isInvalid={authFailed}
+                        required
+                      />
+                    </FloatingLabel>
+                    <Form.Control.Feedback type="invalid" className="invalid-feedback" style={{ display: authFailed ? 'block' : 'none' }}>the username or password is incorrect</Form.Control.Feedback>
+                  </Form.Group>
+                  <Button type="submit" disabled={formik.isSubmitting} variant="outline-primary" className="w-100 mb-3">Войти</Button>
+                </fieldset>
+              </Form>
+            </Card.Body>
+            <Card.Footer className="p-4">
+              <div className="text-center">
+                <span>notAccount</span>
+                {' '}
+                <NavLink to={getPath.signupPagePath()}>signUp</NavLink>
+              </div>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
---
+++ frontend/src/hooks/index.js
@@ -0,0 +1,7 @@
+import { useContext } from 'react'
+import { AuthContext, ChatContext } from '../contexts/index.jsx'
+
+const useAuth = () => useContext(AuthContext)
+const useChat = () => useContext(ChatContext)
+
+export { useAuth, useChat }
