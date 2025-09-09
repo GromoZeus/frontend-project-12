82166dcaeb2d6d3105f3088118338f28793f6135
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-09-09 20:16:15 +0700

    fix

--- frontend/src/components/ChatPage/ChatPage.jsx
+++ frontend/src/components/ChatPage/ChatPage.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Container } from 'react-bootstrap'
+import { Container, Spinner } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import { toast } from 'react-toastify'
 
-import fetchData from '../../thunk/fetchData.js'
+import fetchData from '../../thunk/index.js'
 import { useAuth } from '../../hooks/index.js'
-import { selectChannelsLoading } from '../../slices/channelsState.js'
+import { selectChannelsLoading } from '../../slices/UIState.js'
 import ChannelComponent from './ChannelsComponent.jsx'
 import MessagesComponent from './MessagesComponent.jsx'
 
@@ -30,10 +30,10 @@ const ChatPage = () => {
 
   if (isLoading) {
     return (
-      <Container className="h-100 my-4 overflow-hidden rounded shadow">
-        <div className="row h-100 bg-white flex-md-row">
-          <h1>{t('loading')}</h1>
-        </div>
+      <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">{t('loading')}</span>
+        </Spinner>
       </Container>
     )
   }
