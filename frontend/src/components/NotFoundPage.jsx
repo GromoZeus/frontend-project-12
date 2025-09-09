19e5546bf9c96a069aed2dcc3a2b99aab5249ed3
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-09-09 20:23:44 +0700

    modified:   NotFoundPage.jsx

--- frontend/src/components/NotFoundPage.jsx
+++ frontend/src/components/NotFoundPage.jsx
@@ -1,8 +1,22 @@
-const NotFoundPage = () => (
-  <div id="root">
-    <h2>Unexpected Application Error!</h2>
-    <h3 style={{ fontStyle: 'italic' }}>404 Not Found</h3>
-  </div>
-)
+import { useTranslation } from 'react-i18next'
+
+import getPath from '../path.js'
+import imagePath from '../assets/404.svg'
+
+const NotFoundPage = () => {
+  const { t } = useTranslation()
+
+  return (
+    <div className="text-center">
+      <img alt="Страница не найдена" className="img-fluid h-25" src={imagePath} />
+      <h1 className="h4 text-muted">{t('notFound')}</h1>
+      <p className="text-muted">
+        {t('youCanGo')}
+        {' '}
+        <a href={getPath.chatPagePath()}>{t('toHomePage')}</a>
+      </p>
+    </div>
+  )
+}
 
 export default NotFoundPage
