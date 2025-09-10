2d7b0c7d6732f5733da579d91ab5a25cc120d24f
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-09-10 21:16:22 +0700

    test roollbar

--- frontend/src/roollbar/index.js
+++ frontend/src/roollbar/index.js
@@ -1,10 +1,6 @@
 const rollbarConfig = {
-  accessToken: '3743e65fadcc4da2bc600d8cd635347a737dda703bd35919d3eaa0664a315e7be3639205c98299e3827b52afdb656556',
-  payload: {
-    environment: 'production',
-  },
-  captureUncaught: true,
-  captureUnhandledRejections: true,
+  accessToken: '09273cd4bab547fcabc509b72e4b8277',
+  environment: 'testenv',
 }
 
 export default rollbarConfig
