5fc4f110c814be171465f7c27ea81ee2f80439c4
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-09-10 21:07:19 +0700

    add roollbar config

---
+++ frontend/src/roollbar/index.js
@@ -0,0 +1,10 @@
+const rollbarConfig = {
+  accessToken: '3743e65fadcc4da2bc600d8cd635347a737dda703bd35919d3eaa0664a315e7be3639205c98299e3827b52afdb656556',
+  payload: {
+    environment: 'production',
+  },
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+}
+
+export default rollbarConfig
